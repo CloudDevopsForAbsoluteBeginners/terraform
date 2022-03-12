dd5c069d60cc2040879026e067092d6ce0c58281
Krishnamoorthy <krishnamoorthy.vedi2022@gmail.com>
2022-03-12 18:08:49 +0530

    updated ec2 tf file

--- terraform-scenarios/create-ec2-instance-aws.tf
+++ terraform-scenarios/create-ec2-instance-aws.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "myfirstec2" {
-    ami = "ami-02f47fa62c613afb4" #Amazon linux image
+    ami = "ami-0e0ff68cb8e9a188a"
     instance_type = "t2.micro"
   
 }
 
 provider "aws" {
-  region = "ap-southeast-1" #singapore region
-  access_key = "AKIA5JZ3KH5CKNXVYIZZ"
-  secret_key = "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"
+  region = "ap-south-1"
+  access_key = "AKIA5JZ3KH5CLBPSX6BC"
+  secret_key = "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"
 }
