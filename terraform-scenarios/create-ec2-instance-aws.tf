52009e56756561fd87461005fc14b05e420c5608
CloudDevopsForAbsoluteBeginners <99682395+CloudDevopsForAbsoluteBeginners@users.noreply.github.com>
2022-03-19 11:31:26 +0530

    Update create-ec2-instance-aws.tf

--- terraform-scenarios/create-ec2-instance-aws.tf
+++ terraform-scenarios/create-ec2-instance-aws.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "myfirstec2" {
-    ami = "ami-0e0ff68cb8e9a188a"
+    ami = "ami-0801a1e12f4a9ccc0"
     instance_type = "t2.micro"
   
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = "ap-southeast-1"
   access_key = "your access key"
   secret_key = "your secret key"
 }
